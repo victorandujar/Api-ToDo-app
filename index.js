3defd3fe645ac2ffeef0a1a7b7220295cb37d258
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-13 20:09:24 +0100

    Change naming files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("ToDo.json");
+const router = jsonServer.router("todo.json");
 const port = process.env.PORT || 4000;
 
 server.use(cors());
